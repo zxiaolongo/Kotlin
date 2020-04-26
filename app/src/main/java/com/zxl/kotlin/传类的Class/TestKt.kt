facde97ecd1d78aba55820498b7c79ca619c7281
zxiaolongo <37070112+zxiaolongo@users.noreply.github.com>
2020-04-26 16:39:52 +0800

    Update TestKt.kt

--- app/src/main/java/com/zxl/kotlin/传类的Class/TestKt.kt
+++ app/src/main/java/com/zxl/kotlin/传类的Class/TestKt.kt
@@ -2,12 +2,12 @@ package com.zxl.kotlin.传类的Class
 
 class TestKt{
     //k-java
-    fun test(a:Int){
+    fun test(){
         var jav = ClassKt()
-        jav.test(ClassJava::class.java)a:Int
+        jav.test(ClassJava::class.java)
     }
     //k-kotlin
-    fun test2(){
+    fun test2(a:Int){
         var kt = ClassKt()
         kt.testKt(ClassKt::class.java)
     }
